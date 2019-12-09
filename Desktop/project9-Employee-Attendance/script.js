ff6387872211793482bdb88c28719f9005285e54
Chris Cooper <bloodwolf444@gmail.com>
2019-12-08 19:08:20 -0800

    new changes

--- Desktop/project9-Employee-Attendance/script.js
+++ Desktop/project9-Employee-Attendance/script.js
@@ -1,76 +1,119 @@
-
-
 /*using javascript class syntax to create and manage numerous objects at the same time.
  Class has to be upper case to differenciate from functions and variables*/
 class Player {
-//creates properties of the object we can plug in
- constructor(name,age,online) {
- this.name = name;
- this.age = age;
- this.online = online;
-}
+    //creates properties of the object we can plug in
+    constructor(name,age,online=false) {
+        this.name = name;
+        this.age = age;
+        this.online = online;
+    }
 //creates a method we can use for these objects that do special tasks
 
-onlineStatus() {
+    onlineStatus() {
+        console.log(`${this.name} is ${this.online ? `online` : `offline`} at the moment`)
+    }
 
-  console.log(`${this.name} is ${this.online ? `online` : `offline`} at the moment`)}
-
+    get online() {
+        return this._online
+    }
+    set online(online) {
+        this._online = online
+    }
 }
 
 
 
 //creating a new object from scratch using javascript class syntax. You can create objects very fast this way.
-var player1 = new Player('Chris', '23', true);
-var player2 = new Player('John', '34', true);
-var player3 = new Player('Kelly', '18', false);
-var player4 = new Player('Madison', '25', true);
+const player1 = new Player('Chris', '23');
+const error = new Player('ERROR', 'ERROR');
+const player2 = new Player('John', '34');
+const player3 = new Player('Kelly', '18');
+const player4 = new Player('Madison', '25');
 
 
-var attendance = [player1, player2, player3, player4];
+//storing my objects in a array
+const attendance = [player1, error, player2, player3, player4];
+
+//addding dynamic values
+/*
+const today = new Date();
+const hour = today.getHours();
+const second = today.getSeconds();
+
+for (let i = 0; i < attendance.length; i++){
+
+    if (hour > 19 && hour < 20) {
+        if(player1 && player2 && player3 && player4) {attendance[i]._online = true;}
+    } else {
+        attendance[i]._online = false;
+    }
+
+    if (second > 20 || second < 40) {
+        if(error) {
+            attendance[i]._online = true;}
+    } else {
+        attendance[i]._online = false;
+    }
+}
+*/
 
 
+const playerList = document.getElementById('playerList');
+const output = document.createElement('div');
 
 // Can style elements in the loop
-for (var i = 0; i < attendance.length; i++) {
+for (let i = 0; i < attendance.length; i++) {
 
-//selects the ul element we want to add on to
-  var playerList = document.getElementById('playerList');
+    var playerProfile = document.createElement('UL');
+    playerProfile.className = 'flex-container';
 
- var playerProfile = document.createElement('UL')
- playerProfile.className = 'flex-container'
-//creates li element then places the values of the obects inside of them
+    var node1 = document.createElement('strong');
+    node1.innerHTML = '<strong>' + 'Name:' + '</strong>';
+    node1.className = 'name';
 
-  var node1 = document.createElement('LI');
-   node1.innerHTML = '<li>' + attendance[i].name + '</li>';
-   node1.className = 'profile-name';
+    var node2 = document.createElement('LI');
+    node2.innerHTML = '<li>' + attendance[i].name + '</li>';
+    node2.className = 'profile-name';
 
-   var node2 = document.createElement('LI');
-    node2.innerHTML = '<li>' + attendance[i].age + '</li>';
-    node2.className = 'profile-age';
-
-    var node3 = document.createElement('LI');
-     node3.innerHTML = '<li>' + attendance[i].online + '</li>';
+    var node3 = document.createElement('strong');
+    node3.innerHTML = '<strong>' + 'Age:' + '</strong>';
+    node3.className = 'age';
 
 
-//if they value inside online is true. replace text and style. same for offline
+    var node4 = document.createElement('LI');
+    node4.innerHTML = '<li>' + attendance[i].age + '</li>';
+    node4.className = 'profile-age';
+
+    var node5 = document.createElement('LI');
+    node5.innerHTML = '<li>' + attendance[i].online + '</li>';
+
     if (attendance[i].online  === true) {
-      node3.textContent = 'Online';
-      node3.className = 'style-online';
+        node5.textContent = 'Online';
+        node5.className = 'style-online';
     }
     else {
-      node3.textContent = 'Offline';
-      node3.className = 'style-offline';
+        node5.textContent = 'Offline';
+        node5.className = 'style-offline';
     }
 
-//adds name, age, online list elements to the unordered list
-   playerProfile.appendChild(node1)
-   playerProfile.appendChild(node2)
-   playerProfile.appendChild(node3)
+    playerProfile.appendChild(node1);
+    playerProfile.appendChild(node2);
+    playerProfile.appendChild(node3);
+    playerProfile.appendChild(node4);
+    playerProfile.appendChild(node5);
 
-//add unoredered list to the main element
- playerList.appendChild(playerProfile);
-
+    output.appendChild(playerProfile);
 }
+
+playerList.appendChild(output);
+
+
+
+
+
+
+
+
 var order = false;
 //creating a promise that handles the eventual completion (or failure) of an asynchronus function
 var breakfastPromise = new Promise( (resolve, reject) => {
